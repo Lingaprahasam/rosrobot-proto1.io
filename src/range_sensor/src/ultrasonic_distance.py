753ef20a019ef5422eef854501533ab78f3cebbf
Lingaprahasam <prahas.programming@gmail.com>
2018-12-30 15:47:26 +0100

    updated script

--- src/range_sensor/src/ultrasonic_distance.py
+++ src/range_sensor/src/ultrasonic_distance.py
@@ -1,7 +1,10 @@
 #Libraries
 import RPi.GPIO as GPIO
 import time
- 
+
+# Speed of sound at sea level 343m/s
+SpeedOfSound_sealevel = 34300
+
 #GPIO Mode (BOARD / BCM)
 GPIO.setmode(GPIO.BOARD)
  
@@ -12,12 +15,16 @@ GPIO_ECHO = 12
 #set GPIO direction (IN / OUT)
 GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
 GPIO.setup(GPIO_ECHO, GPIO.IN)
- 
+
+def init_sensor():
+    GPIO.output(GPIO_TRIGGER, False)
+    time.sleep(2)
+
 def distance():
     # set Trigger to HIGH
     GPIO.output(GPIO_TRIGGER, True)
- 
-    # set Trigger after 0.01ms to LOW
+    
+    # set Trigger after 0.01ms to LOW    
     time.sleep(0.00001)
     GPIO.output(GPIO_TRIGGER, False)
  
@@ -36,11 +43,15 @@ def distance():
     TimeElapsed = StopTime - StartTime
     # multiply with the sonic speed (34300 cm/s)
     # and divide by 2, because there and back
-    distance = (TimeElapsed * 34300) / 2
+    distance = (TimeElapsed * SpeedOfSound_sealevel) / 2
  
     return distance
  
 if __name__ == '__main__':
+    # Initialize sensor
+    init_sensor()
+
+    # Measure distance every 1 second time
     try:
         while True:
             dist = distance()
